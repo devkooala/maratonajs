f54701fb03fe472645637d0e0645d8bd2f11892d
Guilhermy <guilhermy.c1@gmail.com>
2020-07-03 11:05:37 -0300

    Model Account created

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
+const {Account} = require("../models");
 const router = express.Router();
 
+const rounds = 10;
+
 router.get("/sign-in", (req, res) => {
-    res.send("OK signed in");
+    res.send("Whatsapp your fucker");
 });
 
-router.get("/sign-up", (req, res) => {
-    res.send("OK signed up");
+router.get("/sign-up", async (req, res) => {
+    const email = "gui@gmail.com";
+    const password = "123";
+
+    const hash = await bcrypt.hash(password, rounds);
+    const result = await Account.create({email, password: hash});
+    res.json(result);  
 });
 
 module.exports = router;
